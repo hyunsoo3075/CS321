4b89a2293be8aeb57a60b9c9fafb525f9536c93d
DrMario64 <lucasalm05@hotmail.com>
2021-04-01 23:37:52 -0400

    post question and answer and knows that there are reactions
    very basic form of accepting questions and answers and prints the author of the original message when it gets a reaction

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const Discord = require('Discord.js')
 const {prefix, token} = require('./config.json')
 const client = new Discord.Client();
@@ -35,4 +36,50 @@ client.on('message', message => {
 });
 
 //keep at end of file
+=======
+const Discord = require('Discord.js')
+const {prefix, token} = require('./config.json')
+const client = new Discord.Client()
+
+const upFilter = reaction => reaction.emoji.name === '⬆️'
+
+var keyValueArr = {}
+var initialized = false
+var memCount
+
+client.once('ready', () => {
+    console.log('Ready!')
+})
+
+client.on('message', message => {
+    if (!initialized) {
+        const list = client.guilds.cache.get('361157955498016788')
+        memCount = list.memberCount;
+        list.members.cache.forEach(member => keyValueArr[member] = 0)
+        //list.members.cache.forEach(member => console.log(member.user.username));
+        //console.log(keyValueArr)
+        initialized = true;
+    }  
+    
+	if (message.content.startsWith(`[Question]`)) {
+        message.react('⬆️')
+            .then(() => message.react('⬇️'))
+            .catch(() => console.error('One of the emojis failed to react.'))
+    } 
+    else if (message.content.startsWith(`[Answer]`)) {
+        message.react('⬆️')
+            .then(() => message.react('⬇️'))
+            .catch(() => console.error('One of the emojis failed to react.'))
+    }
+
+    //counts upvote reactions for 5 seconds
+    /*message.awaitReactions(upFilter, { time: 5000 })
+        .then(collected => collected.map(s => console.log(`Collected ${s.count}`)));*/
+});
+
+client.on("messageReactionAdd", reaction => {
+    console.log(reaction.message.author.username)
+})
+
+>>>>>>> Stashed changes
 client.login(token)
