40dfd9dcfbf9a3e12410cddeb2b478a265727b4a
DrMario64 <lucasalm05@hotmail.com>
2021-04-02 01:49:27 -0400

    links question & answers, adds and subtracts points
    generates a 6 digit 0-9a-z code for each question and allows users to answer questions using that code.  Also adds points if an upvote was added or a downvote was removed and removes points if a downvote was added or an upvote was removed.
    
    Uses functions (structs) instead of dictionaries for storing information

--- index.js
+++ index.js
@@ -1,63 +1,40 @@
-<<<<<<< Updated upstream
-const Discord = require('Discord.js')
-const {prefix, token} = require('./config.json')
-const client = new Discord.Client();
-
-const upFilter = reaction => reaction.emoji.name === '⬆️';
-
-var keyValueArr = {};
-var oneTimeOnly = false;
-
-client.once('ready', () => {
-    console.log('Bot is on!');
-});
-
-client.on('message', message => {
-    if (oneTimeOnly == false) {
-        const guild = client.guilds.cache.get('812773196381683732'); 
-        var count = guild.memberCount;
-        guild.members.cache.forEach(member => keyValueArr[member] = 0);
-        console.log(keyValueArr);
-        oneTimeOnly = true;
-    }   
-    
-	if (message.content.startsWith(`[Question]`)) {
-        message.react('⬆️')
-            .then(() => message.react('⬇️'));
-    } 
-    else if (message.content.startsWith(`[Answer]`)) {
-        message.react('⬆️')
-            .then(() => message.react('⬇️'));
-    }
-
-    //counts upvote reactions for 15 seconds
-    message.awaitReactions(upFilter, { time: 15000 })
-        .then(collected => collected.map(s => console.log(`Collected ${s.count}`)));
-});
-
-//keep at end of file
-=======
 const Discord = require('Discord.js')
 const {prefix, token} = require('./config.json')
 const client = new Discord.Client()
 
 const upFilter = reaction => reaction.emoji.name === '⬆️'
 
-var keyValueArr = {}
+//var keyValueArr = {}
+var questions = []
+var users = []
 var initialized = false
+var ID = 1
 var memCount
+
+function question(ID, answers, topAnswer) {
+    this.ID = ID
+    this.answers = answers
+    this.topAnswer = topAnswer
+}
+
+function user(name, points) {
+    this.name = name
+    this.points = points
+}
 
 client.once('ready', () => {
     console.log('Ready!')
 })
 
 client.on('message', message => {
+    if (message.author.bot) return  //disregard all messages that come from bots (including this one)
     if (!initialized) {
         const list = client.guilds.cache.get('361157955498016788')
         memCount = list.memberCount;
-        list.members.cache.forEach(member => keyValueArr[member] = 0)
+        list.members.cache.forEach(member => users.push(new user(member.user.username), 0))
+        //list.members.cache.forEach(member => keyValueArr[member] = 0)
         //list.members.cache.forEach(member => console.log(member.user.username));
-        //console.log(keyValueArr)
+        //console.log(users)
         initialized = true;
     }  
     
@@ -65,11 +42,28 @@ client.on('message', message => {
         message.react('⬆️')
             .then(() => message.react('⬇️'))
             .catch(() => console.error('One of the emojis failed to react.'))
-    } 
+        var newQuestion = new question((Math.random().toString(36)+'00000000000000000').slice(2, 8), [], 0)
+        questions.push(newQuestion)
+        message.channel.send("Question ID: " + newQuestion.ID + ". Answer by starting your message with [Answer] " + newQuestion.ID)
+        //console.log((Math.random().toString(36)+'00000000000000000').slice(2, 8))
+        //console.log(questions[0].ID)
+    }
+
     else if (message.content.startsWith(`[Answer]`)) {
-        message.react('⬆️')
-            .then(() => message.react('⬇️'))
-            .catch(() => console.error('One of the emojis failed to react.'))
+        var code = message.content.slice(9, 15)
+        //console.log(code)
+        var msg = message.content.slice(15, message.length)
+        console.log(msg)
+        for (var i = 0; i < questions.length; i++) {
+            if (questions[i].ID == code) {
+                questions[i].answers.push(msg)
+                message.react('⬆️')
+                    .then(() => message.react('⬇️'))
+                    .catch(() => console.error('One of the emojis failed to react.'))
+                return
+            }
+        }
+        message.channel.send("incorrect question code. Example usage: '[Answer] 123456 you missed a semicolon on line 8'")
     }
 
     //counts upvote reactions for 5 seconds
@@ -81,5 +75,8 @@ client.on("messageReactionAdd", reaction => {
     console.log(reaction.message.author.username)
 })
 
->>>>>>> Stashed changes
+client.on("messageReactionRemove", reaction => {
+    console.log("reaction removed!")
+})
+
 client.login(token)
