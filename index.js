a05044b089aea26c63a9b9938d2e4670bd8a4d51
ScottEmert <scottemert9@gmail.com>
2021-04-18 16:15:05 -0400

    more functionaility
    added downvote counting and storage, stubs for new commands, and loops for debugging

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const {prefix, token} = require('./config.json')
 const client = new Discord.Client();
 
 const upFilter = reaction => reaction.emoji.name === '⬆️';
+const downFilter = reaction => reaction.emoji.name === '⬇️';
 
 var keyValueArr = {};
 var oneTimeOnly = false;
 
 client.once('ready', () => {
-    console.log('Bot is on!');
+    console.log('Bot is active!');
 });
+
+//print info to console for debugging
+for (key in keyValueArr) {
+    console.log(key, keyValueArr[key]);
+}
+for (val in keyValueArr) {
+    console.log(val, keyValueArr[val]);
+}
 
 client.on('message', message => {
     if (oneTimeOnly == false) {
@@ -28,9 +37,23 @@ client.on('message', message => {
         message.react('⬆️')
             .then(() => message.react('⬇️'));
     }
+    else if (message.content.startsWith(`[ans]`)) {
+        
+    }
+    else if (message.content.startsWith(`[answers]`)) {
 
-    //counts upvote reactions for 15 seconds
+    }
+    else if (message.content.startsWith(`[score]`)) {
+        
+    }
+    else {
+
+    }
+
+    //counts upvote and downvote reactions for 15 seconds
     message.awaitReactions(upFilter, { time: 15000 })
+        .then(collected => collected.map(s => console.log(`Collected ${s.count}`)));
+    message.awaitReactions(downFilter, { time: 15000 })
         .then(collected => collected.map(s => console.log(`Collected ${s.count}`)));
 });
 
